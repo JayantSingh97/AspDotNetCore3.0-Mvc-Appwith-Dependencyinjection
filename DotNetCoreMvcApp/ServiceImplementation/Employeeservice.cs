dea70be1663ee8ce290e7e8964532365b1f39463
Jayant singh <51473212+JayantSingh97@users.noreply.github.com>
2019-10-03 11:42:05 +0530

    Code Updated
    Code Updated

--- DotNetCoreMvcApp/ServiceImplementation/Employeeservice.cs
+++ DotNetCoreMvcApp/ServiceImplementation/Employeeservice.cs
@@ -1,4 +1,4 @@
-﻿using DotNetCoreMvcApp.EmployeeService;
+using DotNetCoreMvcApp.EmployeeService;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -10,43 +10,50 @@ namespace DotNetCoreMvcApp.Models
     public class EmployeeModel
     {
         //Employee model MVC class Data validation with System.ComponentModel.DataAnnotations
-
         public int EmployeeId { get; set; }
+
         [DataType(DataType.Text)]
         [Required(ErrorMessage = "Employee Name is Required")]
         [MinLength(2, ErrorMessage = "Employee Name length should be > 2 characters.")]
         [MaxLength(10, ErrorMessage = "Employee Name length should be < 10 characters")]
         public string FirstName { get; set; }
+
         [DataType(DataType.Text)]
         [Required(ErrorMessage = "Employee LastName is Required")]
         [MinLength(2, ErrorMessage = "Employee LastName length should be > 2 characters.")]
         [MaxLength(10, ErrorMessage = "Employee LastName length should be < 10 characters")]
         public string LastName { get; set; }
+
         [DataType(DataType.Date)]
         [Required(ErrorMessage = "Employee DOB is Required")]
         [MinLength(2, ErrorMessage = "Employee DOB length should be > 2 characters.")]
         [MaxLength(10, ErrorMessage = "Employee DOB length should be < 10 characters")]
         public string DOB { get; set; }
+
         [DataType(DataType.Text)]
         [Required(ErrorMessage = "Employee Country is Required")]
         [MinLength(2, ErrorMessage = "Employee Country length should be > 2 characters.")]
         [MaxLength(10, ErrorMessage = "Employee Country length should be < 10 characters")]
         public string Country { get; set; }
+
         [DataType(DataType.Text)]
         [Required(ErrorMessage = "Employee Department is Required")]
         [MinLength(2, ErrorMessage = "Employee Department length should be > 2 characters.")]
         [MaxLength(10, ErrorMessage = "Employee Department length should be < 10 characters")]
         public string Department { get; set; }
+
         [DataType(DataType.PhoneNumber)]
         [Required(ErrorMessage = "Employee Phone is Required")]
         [MinLength(10, ErrorMessage = "Employee Phone length should be >= 10 characters.")]
         [MaxLength(10, ErrorMessage = "Employee Phone length should be < 10 characters")]
         public string Phone { get; set; }
+
         [DataType(DataType.Text)]
         [Required(ErrorMessage = "Employee ProfileUrl is Required")]
         [MinLength(2, ErrorMessage = "Employee ProfileUrl length should be > 2 characters.")]
         [MaxLength(100, ErrorMessage = "Employee ProfileUrl length should be < 10 characters")]
         public string ProfileUrl { get; set; }
+
         [DataType(DataType.EmailAddress)]
         [Required(ErrorMessage = "Employee Email is Required")]
         [MinLength(2, ErrorMessage = "Employee Email length should be > 2 characters.")]
@@ -58,57 +65,168 @@ namespace DotNetCoreMvcApp.Models
 
     public class Employeeservice : IEmployeeService
     {
-        private List<EmployeeModel> employeemodel = new List<EmployeeModel>();
-        private readonly string img1 = "https://localhost:44386/Pictures/F4.JPG";
-        private readonly string img2 = "https://localhost:44386/Pictures/f3.JPG";
-        private readonly string img3 = "https://localhost:44386/Pictures/m3.Png";
-        private readonly string img4 = "https://localhost:44386/Pictures/m4.JPG";
+        public List<EmployeeModel> Employeemodel => new List<EmployeeModel>();
+        public static string Img1 { get; } = "https://localhost:44386/Pictures/F4.JPG";
+        public static string Img2 { get; } = "https://localhost:44386/Pictures/f3.JPG";
+        public static string Img3 { get; } = "https://localhost:44386/Pictures/m3.Png";
+        public static string Img4 { get; } = "https://localhost:44386/Pictures/m4.JPG";
 
-        private List<EmployeeModel> GetEmployeeContext()
+        private static List<EmployeeModel> GetEmployeeContext()
         {
-            List<EmployeeModel> employees = new List<EmployeeModel>()
+            var employees = new List<EmployeeModel>()
             {
-              new EmployeeModel(){EmployeeId = 100,FirstName="Jhon",LastName="mello", Country="US", Department="IT", DOB = DateTime.Now.ToShortDateString(), Email="example@employee.com", Phone="010009090", ProfileUrl=img3},
-              new EmployeeModel(){EmployeeId = 101,FirstName="Merry",LastName="M", Country="US", Department="HR", DOB = DateTime.Now.ToShortDateString(), Email="example@employee.com", Phone="010049090", ProfileUrl=img1},
-              new EmployeeModel(){EmployeeId = 102,FirstName="George ",LastName="Loe", Country="Russia", Department="IT", DOB = DateTime.Now.ToShortDateString(), Email="example@employee.com", Phone="010059090", ProfileUrl=img4},
-              new EmployeeModel(){EmployeeId = 103, FirstName = "Jacob", LastName = "Joshep", Country = "UK", Department = "HR", DOB = DateTime.Now.ToShortDateString(), Email = "example@employee.com", Phone = "030009090", ProfileUrl = img2 },
-              new EmployeeModel(){EmployeeId = 104,FirstName = "Rakesh",LastName = "Singh", Country = "India", Department = "IT", DOB = DateTime.Now.ToShortDateString(), Email = "example@employee.com", Phone = "410009090", ProfileUrl = img3},
-              new EmployeeModel(){EmployeeId = 105,FirstName = "Jack",LastName = "kom", Country = "Russia", Department = "Admin", DOB = DateTime.Now.ToShortDateString(), Email = "example@employee.com", Phone = "670009090", ProfileUrl = img4},
-              new EmployeeModel(){EmployeeId = 106,FirstName = "Minto",LastName = "tom", Country = "US", Department = "Admin", DOB = DateTime.Now.ToShortDateString(), Email = "example@employee.com", Phone = "0100149090", ProfileUrl = img3},
-              new EmployeeModel(){EmployeeId = 107,FirstName = "Oscar",LastName = "S", Country = "US", Department = "IT", DOB = DateTime.Now.ToShortDateString(), Email = "example@employee.com", Phone = "010789090", ProfileUrl = img3},
-              new EmployeeModel(){EmployeeId = 108,FirstName = "V.K",LastName = "Singh", Country = "India", Department = "Admin", DOB = DateTime.Now.ToShortDateString(), Email = "example@employee.com", Phone = "010000090", ProfileUrl = img4},
-              new EmployeeModel(){EmployeeId = 109,FirstName = "Harry",LastName = "pitter", Country = "UK", Department = "IT", DOB = DateTime.Now.ToShortDateString(), Email = "example@employee.com", Phone = "010077090", ProfileUrl = img3}
-             };
+                new EmployeeModel()
+                {
+                    EmployeeId = 100,
+                    FirstName = "Jhon",
+                    LastName = "mello",
+                    Country = "US",
+                    Department = "IT",
+                    DOB = DateTime.Now.ToShortDateString(),
+                    Email = "example@employee.com",
+                    Phone = "010009090",
+                    ProfileUrl = Img3
+                },
+                new EmployeeModel()
+                {
+                    EmployeeId = 101,
+                    FirstName = "Merry",
+                    LastName = "M",
+                    Country = "US",
+                    Department = "HR",
+                    DOB = DateTime.Now.ToShortDateString(),
+                    Email = "example@employee.com",
+                    Phone = "010049090",
+                    ProfileUrl = Img1
+                },
+                new EmployeeModel()
+                {
+                    EmployeeId = 102,
+                    FirstName = "George ",
+                    LastName = "Loe",
+                    Country = "Russia",
+                    Department = "IT",
+                    DOB = DateTime.Now.ToShortDateString(),
+                    Email = "example@employee.com",
+                    Phone = "010059090",
+                    ProfileUrl = Img4
+                },
+                new EmployeeModel()
+                {
+                    EmployeeId = 103,
+                    FirstName = "Jacob",
+                    LastName = "Joshep",
+                    Country = "UK",
+                    Department = "HR",
+                    DOB = DateTime.Now.ToShortDateString(),
+                    Email = "example@employee.com",
+                    Phone = "030009090",
+                    ProfileUrl = Img2
+                },
+                new EmployeeModel()
+                {
+                    EmployeeId = 104,
+                    FirstName = "Rakesh",
+                    LastName = "Singh",
+                    Country = "India",
+                    Department = "IT",
+                    DOB = DateTime.Now.ToShortDateString(),
+                    Email = "example@employee.com",
+                    Phone = "410009090",
+                    ProfileUrl = Img3
+                },
+                new EmployeeModel()
+                {
+                    EmployeeId = 105,
+                    FirstName = "Jack",
+                    LastName = "kom",
+                    Country = "Russia",
+                    Department = "Admin",
+                    DOB = DateTime.Now.ToShortDateString(),
+                    Email = "example@employee.com",
+                    Phone = "670009090",
+                    ProfileUrl = Img4
+                },
+                new EmployeeModel()
+                {
+                    EmployeeId = 106,
+                    FirstName = "Minto",
+                    LastName = "tom",
+                    Country = "US",
+                    Department = "Admin",
+                    DOB = DateTime.Now.ToShortDateString(),
+                    Email = "example@employee.com",
+                    Phone = "0100149090",
+                    ProfileUrl = Img3
+                },
+                new EmployeeModel()
+                {
+                    EmployeeId = 107,
+                    FirstName = "Oscar",
+                    LastName = "S",
+                    Country = "US",
+                    Department = "IT",
+                    DOB = DateTime.Now.ToShortDateString(),
+                    Email = "example@employee.com",
+                    Phone = "010789090",
+                    ProfileUrl = Img3
+                },
+                new EmployeeModel()
+                {
+                    EmployeeId = 108,
+                    FirstName = "V.K",
+                    LastName = "Singh",
+                    Country = "India",
+                    Department = "Admin",
+                    DOB = DateTime.Now.ToShortDateString(),
+                    Email = "example@employee.com",
+                    Phone = "010000090",
+                    ProfileUrl = Img4
+                },
+                new EmployeeModel()
+                {
+                    EmployeeId = 109,
+                    FirstName = "Harry",
+                    LastName = "pitter",
+                    Country = "UK",
+                    Department = "IT",
+                    DOB = DateTime.Now.ToShortDateString(),
+                    Email = "example@employee.com",
+                    Phone = "010077090",
+                    ProfileUrl = Img3
+                }
+            };
             return employees;
         }
 
         IList<EmployeeModel> IEmployeeService.GetEmployees()
         {
-            return this.GetEmployeeContext();
+            return GetEmployeeContext();
         }
-
 
         IList<EmployeeModel> IEmployeeService.GetEmployeeById(int id)
         {
-            return this.GetEmployeeContext().Where(emp => emp.EmployeeId == id).ToList();
+            return GetEmployeeContext().Where(emp => emp.EmployeeId == id).ToList();
         }
 
         IList<EmployeeModel> IEmployeeService.CreateEmployee(EmployeeModel employee)
         {
             try
             {
-                EmployeeModel emp = new EmployeeModel();
-                emp.DOB = employee.DOB;
-                emp.Email = employee.Email;
-                emp.Phone = employee.Phone;
-                emp.Country = employee.Country;
-                emp.LastName = employee.LastName;
-                emp.FirstName = employee.FirstName;
-                emp.Department = employee.Department;
-                emp.ProfileUrl = employee.ProfileUrl;
-                emp.EmployeeId = employee.EmployeeId;
-                employeemodel.Add(emp);
-                return employeemodel;
+                var emp = new EmployeeModel
+                {
+                    DOB = employee.DOB,
+                    Email = employee.Email,
+                    Phone = employee.Phone,
+                    Country = employee.Country,
+                    LastName = employee.LastName,
+                    FirstName = employee.FirstName,
+                    Department = employee.Department,
+                    ProfileUrl = employee.ProfileUrl,
+                    EmployeeId = employee.EmployeeId
+                };
+                Employeemodel.Add(emp);
+                return Employeemodel;
             }
             catch (Exception e)
             {
@@ -118,37 +236,32 @@ namespace DotNetCoreMvcApp.Models
 
         IList<EmployeeModel> IEmployeeService.UpdateEmployee(EmployeeModel employee)
         {
-            EmployeeModel emp = this.GetEmployeeContext().Where(e => e.EmployeeId == employee.EmployeeId).SingleOrDefault();
-            if (emp != null)
-            {
-                emp.DOB = employee.DOB;
-                emp.Email = employee.Email;
-                emp.Phone = employee.Phone;
-                emp.Country = employee.Country;
-                emp.LastName = employee.LastName;
-                emp.FirstName = employee.FirstName;
-                emp.Department = employee.Department;
-                emp.ProfileUrl = employee.ProfileUrl;
-                emp.EmployeeId = employee.EmployeeId;
-                employeemodel.Add(emp);
-                return employeemodel;
-            }
-            return new List<EmployeeModel>();
+            var emp = GetEmployeeContext().SingleOrDefault(e => e.EmployeeId == employee.EmployeeId);
+            if (emp != null) return new List<EmployeeModel>();
+            emp.DOB = employee.DOB;
+            emp.Email = employee.Email;
+            emp.Phone = employee.Phone;
+            emp.Country = employee.Country;
+            emp.LastName = employee.LastName;
+            emp.FirstName = employee.FirstName;
+            emp.Department = employee.Department;
+            emp.ProfileUrl = employee.ProfileUrl;
+            emp.EmployeeId = employee.EmployeeId;
+            Employeemodel.Add(emp);
+            return Employeemodel;
         }
 
         IList<EmployeeModel> IEmployeeService.DeleteEmployee(int id)
         {
-            EmployeeModel emp = this.GetEmployeeContext().Where(e => e.EmployeeId == id).SingleOrDefault();
-            if (emp != null)
-            {
-                return this.GetEmployeeContext().Where(e => e.EmployeeId != emp.EmployeeId).ToList();
-            }
-            return new List<EmployeeModel>();
+            var emp = GetEmployeeContext().SingleOrDefault(e => e.EmployeeId == id);
+            return emp != null
+                ? GetEmployeeContext().Where(e => e.EmployeeId != emp.EmployeeId).ToList()
+                : new List<EmployeeModel>();
         }
 
         EmployeeModel IEmployeeService.FindEmployee(int id)
         {
-            return this.GetEmployeeContext().Where(e => e.EmployeeId == id).SingleOrDefault();
+            return GetEmployeeContext().SingleOrDefault(e => e.EmployeeId == id);
         }
     }
 }
